0bf4eb4c9489adbcd2ed7da292e604c1a0b7fd8c
Carl Wilkerson <cwilkerson@inphi.com>
2017-04-24 11:02:45 -0700

    Rearranged directory structure.

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Project for all things python3.
 ### Prerequisites
 
 Python3 32bit
+
+Axelrod package
--- simple_tournament.py
+++
@@ -1,8 +0,0 @@
-import axelrod as axl
-
-axl.seed(0)  # Set a seed
-players = [s() for s in axl.demo_strategies]  # Create players
-tournament = axl.Tournament(players)  # Create a tournament
-results = tournament.play()  # Play the tournament
-
-print(results.ranked_names)  # Print the results of the tournament
